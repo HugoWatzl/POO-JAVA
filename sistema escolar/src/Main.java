6d6337d58b9c6a3c5fb7fa36e93e32f9903106f7
Hugo Watzl <143032400+HugoWatzl@users.noreply.github.com>
2025-04-28 16:45:16 -0300

    Update Main.java

--- sistema escolar/src/Main.java
+++ sistema escolar/src/Main.java
@@ -7,25 +7,45 @@ public class Main {
       Curso m1 = new Curso("matematica", 10);
 
       do{
+          System.out.println("  ----- MENU -----");
           System.out.println("Aperte 1 para Matricular : ");
           System.out.println("Aperte 2 para Desmatricular : ");
           System.out.println("Aperte 3 para imprimir da dados : ");
           System.out.println("Aperte 4 para Sair : ");
 
           op = sc.nextInt();
+          sc.nextLine();
 
         switch (op) {
             case 1: System.out.println("Insira o nome do Aluno : ");
-                Curso.Matricular();
+                String nomealu = sc.nextLine();
+
+                System.out.println("Insira a matricula do Aluno : ");
+                String matriculalu = sc.nextLine();
+
+                Aluno novaluno = new Aluno(nomealu,matriculalu);
+                if (m1.Matricular(novaluno)) {
+                    System.out.println("Aluno matriculado com sucesso!");
+                } else {
+                    System.out.println("Não foi possível matricular o aluno. Verifique as vagas.");
+                }
             break;
 
-            case 2: System.out.println("Insira o nome do Aluno : ");
-            Curso.Desmatricular();
+            case 2: System.out.println("Insira a matricula do Aluno : ");
+            String a = sc.nextLine();
+
+            if(m1.Desmatricular(a)){System.out.println("Aluno des1matriculado com sucesso!");
+            }
+            else System.out.println("Aluno não encontrado .");
+
             break;
+
 
             case 3: System.out.println("Insira o nome do Aluno : ");
-            Curso.imprimi();
+            m1.imprimi();
             break;
+
+
             case 4: System.out.println("Encerrando programa : ");
             break;
             default: System.out.println("Opição invalida!Escolha de 1-4.");
@@ -36,3 +56,4 @@ public class Main {
       sc.close();
  }
 }
+
